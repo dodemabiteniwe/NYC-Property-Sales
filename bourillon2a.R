47488c15764cbf20085f751ceccf1df7487840f4
dodemabiteniwe <dodemabiteniwe@gmail.com>
2024-09-28 21:09:36 +0000

    update

--- bourillon2a.R
+++ bourillon2a.R
@@ -10,6 +10,7 @@ library(scales)
 library(ggpubr)
 library(xgboost)
 library(gbm)
+library(janitor)
 
 # Step 1: Data Loading
 temp <- tempfile()
@@ -96,46 +97,175 @@ correlation_table2 <- data.frame(
 print(correlation_table2)
 
 
-# Step 3: Exploratory Data Analysis (EDA)
-nyc_data_filtered <- nyc_data %>% filter(SALE.PRICE >= 100 & SALE.PRICE <= 5000000)
+
+#######################################################################
+#------Exploratory Data Analysis (EDA)----------------------------
+######################################################
+
+nyc_data_filtered <- nyc_data %>% filter(`SALE PRICE` >= 100 & `SALE PRICE` <= 5000000)
 
 # Sale Price Distribution with Density, Mean, and Median
-ggplot(nyc_data_filtered, aes(x = SALE.PRICE)) +
-  geom_histogram(aes(y = ..density..), bins = 50, fill = "blue", alpha = 0.5) +
-  geom_density(color = "red", size = 1) +
-  geom_vline(aes(xintercept = mean(SALE.PRICE, na.rm = TRUE)), color = "green", linetype = "dashed", size = 1) +
-  geom_vline(aes(xintercept = median(SALE.PRICE, na.rm = TRUE)), color = "blue", linetype = "dashed", size = 1) +
-  labs(title = "Distribution of Sale Price", x = "Sale Price", y = "Density")
+ggplot(nyc_data_filtered, aes(x = `SALE PRICE`)) +
+  geom_histogram(aes(y = after_stat(density)), bins = 50, fill = "skyblue", color = "black", alpha = 0.7) +
+  geom_density(color = "red", linewidth = 1) +
+  geom_vline(aes(xintercept = mean(`SALE PRICE`, na.rm = TRUE)), color = "Violet", linetype = "dashed", size = 1) +
+  geom_vline(aes(xintercept = median(`SALE PRICE`, na.rm = TRUE)), color = "blue", linetype = "dashed", size = 1) +
+  labs(title = "Distribution of Sale Price", x = "Sale Price", y = "Density")+
+  theme_minimal() +
+  annotate("text", x = mean(nyc_data_filtered$`SALE PRICE`, na.rm = TRUE), y = 0.0000016, label = "Mean", color ="Violet" , angle = 90, vjust = 1.5) +
+  annotate("text", x = median(nyc_data_filtered$`SALE PRICE`, na.rm = TRUE), y = 0.0000016, label ="Median", color = "blue", angle = 90, vjust = -1)
+ggsave("figs/densitySale.png")
 
 # Box Plot of Sale Price by Borough
-ggplot(nyc_data_filtered, aes(x = BOROUGH, y = SALE.PRICE)) +
-  geom_boxplot(fill = "lightblue") +
-  coord_cartesian(ylim = c(100, 5000000)) + 
-  labs(title = "Sale Price by Borough", x = "Borough", y = "Sale Price")
 
-# Bar Plot for House Price by Top 10 Neighborhoods
-nyc_data_filtered %>%
+# Filter data to include only SALE PRICE values between 100 000 and 5,000,000
+nyc_data_filtered2 <- nyc_data %>% filter(`SALE PRICE` >= 100000 & `SALE PRICE` <= 5000000)
+ggplot(nyc_data_filtered2, aes(x = BOROUGH, y = `SALE PRICE`, fill = BOROUGH)) +
+  geom_boxplot() +labs(title = "Box Plot of Sale Price by Boroughs",x = "Borough",y = "Sale Price")+
+  scale_fill_brewer(palette = "Set3")+ 
+  theme_minimal()+theme(legend.position = "none") 
+ggsave("figs/BxPlotSaleBor.png")
+
+str(nyc_data_filtered2)
+# Box Plot of Sale Price by Month
+ggplot(nyc_data_filtered2, aes(x = sale_month, y = `SALE PRICE`, fill = sale_month)) +
+  geom_boxplot() +
+  labs(title = "Box Plot of Sale Price by Month",
+       x = "Month",
+       y = "Sale Price") +
+  scale_fill_brewer(palette = "Set3") +  
+  theme_minimal() +
+  theme(legend.position = "none")  
+ggsave("figs/BxPlotByMonth.png")
+
+# 6. Sale Count by Month
+sales_count_by_month2 <- nyc_data%>%
+  group_by(sale_month) %>%
+  summarize(Sale_Count = n(), .groups = 'drop')
+
+ggplot(sales_count_by_month2, aes(x = sale_month, y = Sale_Count, fill = sale_month)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Sale Count by Month",
+       x = "Month",
+       y = "Number of Sales") +
+  scale_fill_brewer(palette = "Set3") +  
+  theme_minimal() +
+  theme(legend.position = "none")
+ggsave("figs/SaleByMonth.png")
+
+# 5. Plot Average Land Square Feet of Properties in Each Borough
+nyc_data_filtered2 %>%
+  group_by(BOROUGH) %>%
+  summarise(avg_land_sqft = mean(`LAND SQUARE FEET`, na.rm = TRUE), .groups = 'drop') %>%
+  ggplot(aes(x = BOROUGH, y = avg_land_sqft, size = avg_land_sqft, color = BOROUGH)) +
+  geom_point(alpha = 0.7) + scale_size_continuous(range = c(8, 15))+
+  labs(title = "Average Land Square Feet by Borough", x = "Borough", y = "Average Land Square Feet")+
+  theme_minimal() + theme(legend.position = "right")
+ggsave("figs/avg_land_sqft.png")
+
+# 6. Bar Plot for House Price by Top 10 Neighborhoods
+# Group by neighborhood and calculate the number of sales and average sale price
+nyc_data_filtered2%>%
   group_by(NEIGHBORHOOD) %>%
-  summarise(avg_price = mean(SALE.PRICE, na.rm = TRUE)) %>%
-  arrange(desc(avg_price)) %>%
-  head(10) %>%
-  ggplot(aes(x = reorder(NEIGHBORHOOD, -avg_price), y = avg_price)) +
-  geom_bar(stat = "identity", fill = "steelblue") +
-  labs(title = "Top 10 Neighborhoods by House Price", x = "Neighborhood", y = "Average Sale Price") +
+  summarize(Number_of_Sales = n(),
+            Avg_Sale_Price = mean(`SALE PRICE`, na.rm = TRUE), .groups = 'drop')%>%
+  arrange(desc(Number_of_Sales)) %>%
+  head(10)%>%
+  ggplot(aes(x = reorder(NEIGHBORHOOD, Number_of_Sales), y = Avg_Sale_Price, fill = NEIGHBORHOOD)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() +  # Flip coordinates for better readability
+  labs(title = "Top 10 Neighborhoods by Number of Sales and Average Sale Price",
+       x = "Neighborhood",
+       y = "Average Sale Price") +
+  theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+
+
 # Correlation Matrix Heatmap
-numeric_columns <- nyc_data_filtered %>%
+nyc_data_reduce <- nyc_data %>%
+  select(-c(LOT, `YEAR BUILT`, sale_year, building_age,`TOTAL UNITS`,`RESIDENTIAL UNITS`))
+
+nyc_data_reduce2 <- nyc_data %>%
+  select(-c(LOT, `YEAR BUILT`, sale_year, building_age,`TOTAL UNITS`,`RESIDENTIAL UNITS`))
+
+
+
+numeric_columns <- nyc_data_reduce2 %>%
   select_if(is.numeric) %>%
   cor(use = "complete.obs")
 corrplot(numeric_columns, method = "color", tl.cex = 0.7, number.cex = 0.7, addCoef.col = "black", round = 2)
 
 # Step 4: Machine Learning
+# Clean column names to be lowercase and without spaces
+nyc_data_reduce2 <- nyc_data_reduce2 %>%
+  clean_names()
+colnames(nyc_data_reduce2)
+nyc_data_reduce2 <- nyc_data_reduce2 %>%
+  select(-c(tax_class_at_present, neighborhood, building_class_at_time_of_sale, sale_month))
+
+
 
 # One Hot Encoding
-nyc_data_encoded <- nyc_data_filtered %>%
+nyc_data_encoded <- nyc_data_reduce2 %>%
   model.matrix(~.-1, .) %>%
   as.data.frame()
+colnames(nyc_data_encoded)
+
+# Split data into training and testing
+set.seed(123)
+trainIndex <- createDataPartition(nyc_data_encoded$sale_price, p = .8, list = FALSE)
+train_data <- nyc_data_encoded[trainIndex, ]
+test_data <- nyc_data_encoded[-trainIndex, ]
+
+# Model Training with Various Algorithms
+# Define the control for training
+train_control <- trainControl(method = "cv", number = 2)
+
+# Train a linear regression model
+linear_model <- train(sale_price ~ ., data = train_data, method = "lm", trControl = train_control)
+summary(linear_model)
+
+# Predict on test data
+linear_predictions <- predict(linear_model, newdata = test_data)
+
+# Evaluate model performance
+linear_rmse <- RMSE(linear_predictions, test_data$sale_price)
+cat("Linear Regression RMSE:", linear_rmse, "\n")
+
+# Train a Random Forest model
+rf_model <- train(sale_price ~ ., data = train_data, method = "rf", trControl = train_control)
+rf_predictions <- predict(rf_model, newdata = test_data)
+rf_rmse <- RMSE(rf_predictions, test_data$sale_price)
+cat("Random Forest RMSE:", rf_rmse, "\n")
+
+# Train a SVM model
+svm_model <- train(sale_price ~ ., data = train_data, method = "svmRadial", trControl = train_control)
+ svm_predictions <- predict(svm_model, newdata = test_data)
+svm_rmse <- RMSE(svm_predictions, test_data$sale_price)
+cat("SVM RMSE:", svm_rmse, "\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Two Approaches: Original Sale Price & Log Sale Price
 nyc_data_encoded$LOG.SALE.PRICE <- log(nyc_data_encoded$SALE.PRICE)
